155bf69b6e02389c60a72b44c1bfa894de360441
gabriel eloy <gabrieleloy@pop-os.localdomain>
2021-10-04 07:13:45 -0300

    created redux implementation

--- App.js
+++ App.js
@@ -1,68 +1,14 @@
-import React, {useState} from 'react';
-import {View, Text, StyleSheet, TextInput} from 'react-native';
-import RoundButton from './components/RoundButton';
+import React from 'react';
+import Counter from './pages/Counter';
+import {Provider} from 'react-redux';
+import store from './redux/store';
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const [increment, setIncrement] = useState('1');
-
-  const handleIncrement = () => setCount(count + Number(increment));
-  const handleDecrement = () => setCount(count - Number(increment));
-
-  const handleChangeText = (number) => setIncrement(number);
-
   return (
-    <View style={styles.screenContainer}>
-      <View style={styles.counterContainer}>
-        <RoundButton onPress={handleDecrement}>-</RoundButton>
-        <Text style={styles.text}>{count}</Text>
-        <RoundButton onPress={handleIncrement}>+</RoundButton>
-      </View>
-      <View style={styles.incrementContainer}>
-        <Text style={[styles.text, styles.incrementText]}>Current increment: </Text>
-        <TextInput
-          style={styles.input}
-          onChangeText={handleChangeText}
-          value={increment}
-          placeholder="increment"
-          keyboardType="numeric"
-        />
-      </View>
-    </View>
+    <Provider store={store}>
+      <Counter />
+    </Provider>
   );
 };
-
-const styles = StyleSheet.create({
-  screenContainer: {flex: 1, backgroundColor: '#282a36'},
-  counterContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 130,
-    marginTop: 50,
-    flexDirection: 'row',
-  },
-  text: {
-    color: '#f8f8f2',
-    fontSize: 20,
-    marginHorizontal: 30,
-  },
-  incrementContainer: {
-    flexDirection: 'row',
-    alignItems: 'flex-end',
-    justifyContent: 'space-evenly',
-  },
-  input: {
-    borderBottomWidth: 1,
-    borderColor: '#6272a4',
-    textAlign: 'center',
-    color: '#bd93f9',
-    fontSize: 24,
-    flex: 2,
-    transform: [{translateX: -15}]
-  },
-  incrementText: {
-    flex: 2
-  }
-});
 
 export default App;
